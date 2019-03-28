9a1896f1a7295005fa82d292b01959b24a392332
rfitkin90 <rfitkin90@gmail.com>
2019-03-28 06:11:08 -0700

    complicated mvc

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import API from './utils/API';
 
 class App extends Component {
 
-
-  componentDidMount() {
-    this.loadBooks();
-  }
-
-  loadBooks = () => {
-    API.getBooks()
-      .then(res =>
-        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-      )
-      .catch(err => console.log(err));
-  };
-
-
   render() {
     return (
-      <div className="App">
-        hi
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path='/' component={Books} />
+            <Route exact path='/books' component={Books} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
